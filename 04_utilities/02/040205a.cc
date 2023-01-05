7f358ea0e8eb7adcfe5ac557be10795b2879b8d1
szyhdev <szyuanhua@163.com>
2023-01-05 09:33:09 +0800

    add chapter 04 source code

---
+++ 04_utilities/02/040205a.cc
@@ -0,0 +1,43 @@
+// g++ 040205a.cc -o 040205a -Wno-deprecated-declarations
+
+#include <iostream>
+#include <memory>
+using namespace std;
+
+/* define output operator for auto_ptr
+ * - print object value or NULL
+ */
+template <class T>
+ostream& operator <<(ostream& strm, const auto_ptr<T>& p)
+{
+    // does p own an object ?
+    if (p.get() == NULL) {
+        strm << "NULL";  // NO: print NULL
+    }
+    else {
+        strm << *p;      // YES: print the object
+    }
+    return strm;
+}
+
+int main()
+{
+    auto_ptr<int> p(new int(42));
+    auto_ptr<int> q;
+
+    cout << "after initialization:" << endl;
+    cout << " p: " << p << endl;
+    cout << " q: " << q << endl;
+
+    q = p;
+    cout << "after assigning auto pointers:" << endl;
+    cout << " p: " << p << endl;
+    cout << " q: " << q << endl;
+
+    *q += 13;                   // change value of the object q owns
+    p = q;
+    cout << "after change and reassignment:" << endl;
+    cout << " p: " << p << endl;
+    cout << " q: " << q << endl;
+}
+
