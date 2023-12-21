c41d451a283aaf9e354c732117d229d05c88cca2
szyhua <szyuanhua@163.com>
2023-12-21 20:13:07 +0800

    update chapter 07.03 source code

---
+++ 07_stl_iterators/0303_swap1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+#include "../05_standard_template_library/0700_print.hpp"
+
+using namespace std;
+
+int main()
+{
+    list<int> coll;
+
+    // insert elements from 1 to 9
+    for (int i = 1; i <= 9; ++i) {
+        coll.push_back(i);
+    }
+
+    PRINT_ELEMENTS(coll);
+
+    // swap first and second value
+    iter_swap(coll.begin(), ++coll.begin());
+
+    PRINT_ELEMENTS(coll);
+
+    // swap first and last value
+    iter_swap(coll.begin(), --coll.end());
+
+    PRINT_ELEMENTS(coll);
+
+    return 0;
+}
