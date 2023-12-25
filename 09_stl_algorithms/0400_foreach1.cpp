12a27714d9abf6176baf85122d6602a778ba704a
szyhua <szyuanhua@163.com>
2023-12-25 11:55:00 +0800

    update chapter 09.04 source code

---
+++ 09_stl_algorithms/0400_foreach1.cpp
@@ -0,0 +1,22 @@
+#include "0300_algostuff.hpp"
+
+using namespace std;
+
+// function called for each element
+void print(int elem)
+{
+    cout << elem << ' ';
+}
+
+int main()
+{
+    vector<int> coll;
+    INSERT_ELEMENTS(coll, 1, 9);
+
+    // call print() for each element
+    for_each(coll.begin(), coll.end(),  // range
+            print);                     // operation
+    cout << endl;
+
+    return 0;
+}
