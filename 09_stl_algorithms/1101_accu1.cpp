1c8fad9b3f0b3d54bf78da9251388861df0f823b
szyhua <szyuanhua@163.com>
2023-12-28 14:38:07 +0800

    update chapter 09.11 source code

---
+++ 09_stl_algorithms/1101_accu1.cpp
@@ -0,0 +1,31 @@
+#include "0300_algostuff.hpp"
+
+using namespace std;
+
+int main()
+{
+    vector<int> coll;
+    INSERT_ELEMENTS(coll, 1, 9);
+
+    PRINT_ELEMENTS(coll);
+
+    // process sum of elements
+    cout << "sum: " << accumulate(coll.begin(), coll.end(),  // range
+            0) << endl;                                      // initial value
+
+    // process sum of elements less 100
+    cout << "sum: " << accumulate(coll.begin(), coll.end(),  // range
+            -100) << endl;                                   // initial value
+
+    // process product of elements
+    cout << "product: " << accumulate(coll.begin(), coll.end(),  // range
+            1,                                                   // initial value
+            multiplies<int>()) << endl;                          // operation
+
+    // process product of elements (use 0 as initial value)
+    cout << "product: " << accumulate(coll.begin(), coll.end(),  // range
+            0,                                                   // initial value
+            multiplies<int>()) << endl;                          // operation
+
+    return 0;
+}
---
+++ 09_stl_algorithms/1102_adjdiff1.cpp
@@ -0,0 +1,30 @@
+#include "0300_algostuff.hpp"
+
+using namespace std;
+
+int main()
+{
+    deque<int> coll;
+    INSERT_ELEMENTS(coll, 1, 6);
+
+    PRINT_ELEMENTS(coll);
+
+    // print all differences between elements
+    adjacent_difference(coll.begin(), coll.end(),  // source
+            ostream_iterator<int>(cout, " "));     // destination
+    cout << endl;
+
+    // print all sums with the predecessors
+    adjacent_difference(coll.begin(), coll.end(),  // source
+            ostream_iterator<int>(cout, " "),      // destination
+            plus<int>());                          // operation
+    cout << endl;
+
+    // print all products between elements
+    adjacent_difference(coll.begin(), coll.end(),  // source
+            ostream_iterator<int>(cout, " "),      // destination
+            multiplies<int>());                    // operation
+    cout << endl;
+
+    return 0;
+}
