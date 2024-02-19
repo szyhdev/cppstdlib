939bab5d2537150460846133743ae2f54e6d5c70
szyhdev <szyuanhua@163.com>
2024-02-19 19:20:19 +0800

    update chapter 13.09 source code

---
+++ 13_input_output_using_stream_classes/0901_cat1.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+/* for all file names passed as command-line arguments
+ * - open, print contents, and close file
+ */
+int main(int argc, char *argv[])
+{
+    ifstream file;
+
+    // for all command-line arguments
+    for (int i = 1; i < argc; ++i) {
+        // open file
+        file.open(argv[i]);
+
+        // write file contents to cout
+        char c;
+        while (file.get(c)) {
+            cout.put(c);
+        }
+
+        // clear eofbit and failbit set due to end-of-file
+        file.clear();
+
+        // close file
+        file.close();
+    }
+
+    return 0;
+}
